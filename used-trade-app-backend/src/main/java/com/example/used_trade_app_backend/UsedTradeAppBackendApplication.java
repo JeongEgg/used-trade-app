3a1ef36e87088d1e9e7a8846a74afb2431f14507
JeongBanSeok <wibalislfkas@naver.com>
2024-11-01 22:11:45 +0900

    프로젝트 생성

---
+++ used-trade-app-backend/src/main/java/com/example/used_trade_app_backend/UsedTradeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.used_trade_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradeAppBackendApplication.class, args);
+	}
+
+}
---
+++ used-trade-app-backend/src/test/java/com/example/used_trade_app_backend/UsedTradeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.used_trade_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradeAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
