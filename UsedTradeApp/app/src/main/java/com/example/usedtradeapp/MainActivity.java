5d8421d27756975f2f627e7f6affa4aa4564b6ca
JeongBanSeok <wibalislfkas@naver.com>
2024-11-02 18:17:17 +0900

    안드로이드, 스프링 서버의 api 통신 테스트해보기

--- UsedTradeApp/app/src/main/java/com/example/usedtradeapp/MainActivity.java
+++ UsedTradeApp/app/src/main/java/com/example/usedtradeapp/MainActivity.java
@@ -1,37 +1,64 @@
 package com.example.usedtradeapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.example.usedtradeapp.api.CRUDInterface;
+import com.example.usedtradeapp.dto.MessageDto;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    MessageDto messageDto;
+    CRUDInterface crudInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnGet = findViewById(R.id.btn_get);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getMessage();
+            }
+        });
+    }
+
+    private void getMessage() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        crudInterface = retrofit.create(CRUDInterface.class);
+        Call<MessageDto> call = crudInterface.getMessage();
+        call.enqueue(new Callback<MessageDto>() {
+            @Override
+            public void onResponse(Call<MessageDto> call, Response<MessageDto> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                messageDto = response.body();
+                System.out.println(messageDto.toString());
+            }
+
+            @Override
+            public void onFailure(Call<MessageDto> call, Throwable throwable) {
+
+            }
+        });
     }
 }
