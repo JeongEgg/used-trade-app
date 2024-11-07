c9a696420772e100b5da24fe9cf458ce91c0ecae
JeongBanSeok <wibalislfkas@naver.com>
2024-11-07 23:58:35 +0900

    안드로이드 레이아웃 개발3(상품 검색 페이지)

--- UsedTradeApp/app/src/main/java/com/example/usedtradeapp/fragment/HomeFragment.java
+++ UsedTradeApp/app/src/main/java/com/example/usedtradeapp/fragment/HomeFragment.java
@@ -1,24 +1,56 @@
 package com.example.usedtradeapp.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.example.usedtradeapp.R;
 import com.example.usedtradeapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private String[] sortOptions = {"낮은 가격순", "높은 가격순", "최신 순"};
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
 
-        return binding.getRoot();
+        Button sortButton = rootView.findViewById(R.id.btn_sort);
+        ListView listView = rootView.findViewById(R.id.listview_searched);
+
+        sortButton.setOnClickListener(view -> showSortDialog());
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = (String) parent.getItemAtPosition(position);
+        });
+
+        return rootView;
+    }
+
+    private void showSortDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("분류 선택")
+                .setItems(sortOptions, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String selectedSort = sortOptions[which];
+                        Toast.makeText(getContext(), "선택된 분류방법: " + selectedSort, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
