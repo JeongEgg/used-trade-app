308f8c7dc651a67c1e58a133423bb344b692ee36
JeongBanSeok <wibalislfkas@naver.com>
2024-11-08 13:26:07 +0900

    안드로이드 레이아웃 개발4(프로필 수정 페이지)

--- UsedTradeApp/app/src/main/java/com/example/usedtradeapp/fragment/ProfileFragment.java
+++ UsedTradeApp/app/src/main/java/com/example/usedtradeapp/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.usedtradeapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.usedtradeapp.R;
+import com.example.usedtradeapp.activity.ProfileActivity;
 import com.example.usedtradeapp.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -19,7 +22,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
 
-        return binding.getRoot();
+        rootView.findViewById(R.id.layout_profile_revise).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
